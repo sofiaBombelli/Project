16a426c4f2e91201e1c46be5255ce475b97b3e37
Equipo\Sofia <sofiambombelli@gmail.com>
2026-03-08 22:21:50 -0300

    fade intro juego

---
+++ Assets/Scripts/fadeMapa.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine.SceneManagement;
+
+public class fadeMapa : MonoBehaviour
+{
+    public CanvasGroup canvasGroup;
+    public float fadeDuration = 2f;
+
+    void Start()
+    {
+        StartCoroutine(FadeOut());
+    }
+
+    IEnumerator FadeOut()
+    {
+        yield return new WaitForSeconds(5);
+        float elapsedTime = 0f;
+        float startAlpha = canvasGroup.alpha;
+        float targetAlpha = 0f;
+
+        while (elapsedTime < fadeDuration)
+        {
+            elapsedTime += Time.deltaTime;
+            canvasGroup.alpha = Mathf.Lerp(startAlpha, targetAlpha, elapsedTime / fadeDuration);
+            yield return null;
+        }
+
+        canvasGroup.alpha = targetAlpha;
+    }
+}
